wepow/mongoid_taggable

commit 34c65abb5afae72d6026bbce90553d96de5d1ddb

split strings within the input array

lib/mongoid/taggable.rb
@@ -130,15 +130,22 @@ module Mongoid::Taggable
     # Helper method to convert a String to an Array based on the
     # configured tag separator.
     def convert_string_tags_to_array(_tags)
-      (_tags).split(tags_separator).map(&:strip)
+      (_tags).split(tags_separator)
     end
 
     # Define modifier for the configured tag field name that overrides
     # the default to transparently convert tags given as a String.
     def define_tag_field_accessors(name)
       define_method "#{name}_with_taggable=" do |values|
-        values = convert_string_tags_to_array(values) if values.is_a? String
-        send("#{name}_without_taggable=", values.reject(&:blank?))
+        case values
+        when String
+          values = convert_string_tags_to_array(values)
+        when Array
+          values.dup.each do |value|
+            values = values - value.to_a | convert_string_tags_to_array(value)
+          end
+        end
+        send("#{name}_without_taggable=", values.map(&:strip).reject(&:blank?))
       end
       alias_method_chain "#{name}=", :taggable
     end

spec/mongoid/taggable_spec.rb
@@ -69,6 +69,11 @@ describe Mongoid::Taggable do
       model.tags = ["some", "", "new", "", "tag"]
       model.tags.should == %w[some new tag]
     end
+
+    it "should split any string within the array" do
+      model.tags = ["favorite", "colors", "blue, green"]
+      model.tags.should == %w[favorite colors blue green]
+    end
   end
 
   context "with customized tag field name" do

commit 73c4e09c8258939901de8a175c9d87c86be8e9bd

34c65abb broke private API for tagged_with

lib/mongoid/taggable.rb
@@ -132,7 +132,7 @@ module Mongoid::Taggable
     # Helper method to convert a String to an Array based on the
     # configured tag separator.
     def convert_string_tags_to_array(_tags)
-      (_tags).split(tags_separator)
+      (_tags).split(tags_separator).map(&:strip).reject(&:blank?)
     end
 
     def define_tag_field_accessors(name)
@@ -145,7 +145,7 @@ module Mongoid::Taggable
         when Array
           values = values.inject([]) { |final, value| final.concat convert_string_tags_to_array(value) }
         end
-        send("#{name}_without_taggable=", values.map(&:strip).reject(&:blank?))
+        send("#{name}_without_taggable=", values)
       end
       alias_method_chain "#{name}=", :taggable
 

spec/mongoid/taggable_spec.rb
@@ -210,7 +210,7 @@ describe Mongoid::Taggable do
     end
 
     it "returns all tags with tags string input" do
-      MyModel.tagged_with("tag2,tag1").should == [models.first]
+      MyModel.tagged_with("tag2, tag1").should == [models.first]
     end
 
     it "can be chained with other criteria" do